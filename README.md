5dde6ba6fc7fc8a6c2f633839fbca2a1f96ad695
kalrashid15 <kalrashid.bk@gmail.com>
2019-12-23 04:15:34 +0000

    fixes

--- README.md
+++ README.md
@@ -1,18 +1,3 @@
-<<<<<<< HEAD
 # mutated_batch
 
 Group project for Computational Intelligence for Optimisation module at NOVA IMS
-=======
-# Data Visualisation Project
-
-## Group: 
-Cave Artists
-
-## Members: 
-Kauser Al Rashid (M20190543) <br>
-Norayr Meliksetyan (M20190684) <br>
-Pedro Reis (M20180428)
-
-
-
->>>>>>> 8217244e88f4d5e4edb298ada79b98bcbbf14c5d
--- app.py
+++ app.py
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-
 from flask import Flask, render_template, request, url_for, redirect, session
 
 #--------------------------------------------------------------------------------------------------
@@ -24,18 +22,3 @@ def index():
 if __name__ == '__main__':
     #app_dash.run_server(debug = True)
     app.run(debug = True) 
-=======
-import os
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route('/')
-def hello():
-    return 'Data Viz Project!'
-
-if __name__ == '__main__':
-    # Bind to PORT if defined, otherwise default to 5000.
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
->>>>>>> 8217244e88f4d5e4edb298ada79b98bcbbf14c5d
